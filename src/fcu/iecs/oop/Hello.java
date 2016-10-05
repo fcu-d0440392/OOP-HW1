bca96f1500031f89ad9518b903c906bbb19123ae
Hunter <duck55321@gmail.com>
2016-10-05 17:16:30 +0800

    Homework1最終版

--- src/fcu/iecs/oop/Hello.java
+++ src/fcu/iecs/oop/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 		
 		String lyrics = "Let \"her\" go!Let \"her\" go!Can't hold \"her\" back anymore" ;
+		
 		System.out.println(lyrics) ;
 		// TODO Auto-generated method stub
 	}
