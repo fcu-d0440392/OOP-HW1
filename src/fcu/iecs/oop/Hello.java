258c9c4ef04bd3160db96b4844b5bdd44591e8c5
Hunter <duck55321@gmail.com>
2016-10-05 15:50:48 +0800

    第一版程式碼

---
+++ src/fcu/iecs/oop/Hello.java
@@ -0,0 +1,12 @@
+package fcu.iecs.oop;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World") ;
+		// TODO Auto-generated method stub
+
+	}
+
+}
